1dbd2b01620577b19e6f65b7d9ed115033b25a63
martinsekd <201710685@post.au.dk>
2019-11-18 10:44:46 +0100

    new project

---
+++ Lab10/Lab10/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab10.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab10.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private List<measurement> liste = new List<measurement>();
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post([Bind("temperature","humidity","pressure")] measurement value)
+        {
+            
+            return value.humidity + " " + value.temperature + " " + value.pressure;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Lab10/Lab10/Model/measurement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab10.Model
+{
+        public class measurement
+    {
+        public int measurementId { get; set; }
+        public DateTime dato { get; set; }
+        public double temperature { get; set; }
+        public double humidity { get; set; }
+        public double pressure { get; set; }
+    }
+}
